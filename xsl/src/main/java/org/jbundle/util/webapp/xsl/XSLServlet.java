4375e0e56b509556ee7edd58bb4ce2a757a537ed
doncorley <don@tourgeek.com>
2011-04-23 20:23:02 -0700

    Make xsl servlet work

--- xsl/src/main/java/org/jbundle/util/webapp/xsl/XSLServlet.java
+++ xsl/src/main/java/org/jbundle/util/webapp/xsl/XSLServlet.java
@@ -7,17 +7,30 @@ package org.jbundle.util.webapp.xsl;
  *      don@tourgeek.com
  */
 
+import java.io.FileReader;
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.jbundle.util.webapp.base.BaseServlet;
 
+
 /**
- * RedirectServlet
+ * XSLServlet
+ * params:
+ * source = Path to source document.
+ * stylesheet = Path to stylesheet document.
  * 
  * This is the xsl servlet.
  */
@@ -25,6 +38,10 @@ public class XSLServlet extends BaseServlet
 {
 	private static final long serialVersionUID = 1L;
 
+	public XSLServlet()
+	{
+		super();
+	}
 	/**
      * returns the servlet info
      */ 
@@ -59,6 +76,30 @@ public class XSLServlet extends BaseServlet
     public void service(HttpServletRequest req, HttpServletResponse res) 
         throws ServletException, IOException
     {
-      super.service(req, res);
+    	String sourceFileName = req.getParameter("source");
+    	String styleSheetFileName = req.getParameter("stylesheet");
+    	FileReader sourceFileReader = new FileReader(sourceFileName);
+    	FileReader stylesheetFileReader = new FileReader(styleSheetFileName);
+    	
+    	ServletOutputStream outStream = res.getOutputStream();
+    	
+        try {
+            StreamSource source = new StreamSource(sourceFileReader);
+
+            Result result = new StreamResult(outStream);
+
+            TransformerFactory tFact = TransformerFactory.newInstance();
+            StreamSource streamTransformer = new StreamSource(stylesheetFileReader);
+            
+            Transformer transformer = tFact.newTransformer(streamTransformer);
+
+            transformer.transform(source, result);
+        } catch (TransformerConfigurationException ex)    {
+            ex.printStackTrace();
+        } catch (TransformerException ex) {
+            ex.printStackTrace();
+        }                    
+    	
+    	super.service(req, res);
     }
 }
