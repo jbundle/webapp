84ad824bd0f3378cc0b85d48a7154c019466ac17
doncorley <don@tourgeek.com>
2011-08-31 15:56:40 -0700

    Moved OSGi logic up to OSGiFileServlet

--- osgi/src/main/java/org/jbundle/util/webapp/osgi/BaseOsgiServlet.java
+++ osgi/src/main/java/org/jbundle/util/webapp/osgi/BaseOsgiServlet.java
@@ -3,14 +3,10 @@ package org.jbundle.util.webapp.osgi;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
 import java.util.Dictionary;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.jbundle.util.osgi.finder.ClassServiceUtility;
 
 
 /**
@@ -91,56 +87,6 @@ public abstract class BaseOsgiServlet extends HttpServlet /*JnlpDownloadServlet*
         if (value == null)
                 value = defaultValue;
         return value;
-    }
-    /**
-     * Send this is resource to the response stream.
-     * @param request
-     * @param response
-     * @return
-     * @throws IOException
-     */
-    public boolean getResourceFile(HttpServletRequest request, HttpServletResponse response, boolean useOSGiLookup) throws IOException
-    {
-        URL url = null;
-        if (!useOSGiLookup)
-        {
-            String path = request.getPathInfo();
-            if (path == null)
-                return false;
-            if (!path.startsWith("/"))
-                path = "/" + path;  // Must start from root
-        	url = this.getClass().getResource(path);
-        }
-        else
-        {
-        	String contextPath = this.getServletContext().getContextPath();
-            String path = request.getRequestURI();
-            if ((contextPath != null) && (contextPath.length() > 0))
-            	if (path.startsWith(contextPath))
-            		path = path.substring(contextPath.length());
-            if (path.startsWith("/"))
-            	path = path.substring(1);	// Can't start from root
-            
-            try {
-                url = ClassServiceUtility.getClassService().getResourceURL(path, null, this.getClass().getClassLoader());
-            } catch (RuntimeException e) {
-                e.printStackTrace();    // ???
-            }        	
-        }
-        if (url == null)
-            return false;   // Not found
-        InputStream inStream = null;
-        try {
-            inStream = url.openStream();
-        } catch (Exception e) {
-            return false;   // Not found
-        }
-
-        // Todo may want to add cache info (using bundle lastModified).
-        OutputStream writer = response.getOutputStream();
-        copyStream(inStream, writer, true); // Ignore errors, as browsers do weird things
-        writer.close();
-        return true;
     }
 
 	static final int BUFFER = 2048;
--- osgi/src/main/java/org/jbundle/util/webapp/osgi/OSGiFileServlet.java
+++ osgi/src/main/java/org/jbundle/util/webapp/osgi/OSGiFileServlet.java
@@ -1,10 +1,15 @@
 package org.jbundle.util.webapp.osgi;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jbundle.util.osgi.finder.ClassServiceUtility;
 
 /**
  * A very simple servlet that just serves up resources from the OSGi bundle classpaths.
@@ -23,4 +28,54 @@ public class OSGiFileServlet extends BaseOsgiServlet
 		if (!fileFound)
 		    resp.sendError(HttpServletResponse.SC_NOT_FOUND, "File not found");
 	}
+    /**
+     * Send this is resource to the response stream.
+     * @param request
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    public boolean getResourceFile(HttpServletRequest request, HttpServletResponse response, boolean useOSGiLookup) throws IOException
+    {
+        URL url = null;
+        if (!useOSGiLookup)
+        {
+            String path = request.getPathInfo();
+            if (path == null)
+                return false;
+            if (!path.startsWith("/"))
+                path = "/" + path;  // Must start from root
+        	url = this.getClass().getResource(path);
+        }
+        else
+        {
+        	String contextPath = this.getServletContext().getContextPath();
+            String path = request.getRequestURI();
+            if ((contextPath != null) && (contextPath.length() > 0))
+            	if (path.startsWith(contextPath))
+            		path = path.substring(contextPath.length());
+            if (path.startsWith("/"))
+            	path = path.substring(1);	// Can't start from root
+            
+            try {
+                url = ClassServiceUtility.getClassService().getResourceURL(path, null, this.getClass().getClassLoader());
+            } catch (RuntimeException e) {
+                e.printStackTrace();    // ???
+            }        	
+        }
+        if (url == null)
+            return false;   // Not found
+        InputStream inStream = null;
+        try {
+            inStream = url.openStream();
+        } catch (Exception e) {
+            return false;   // Not found
+        }
+
+        // Todo may want to add cache info (using bundle lastModified).
+        OutputStream writer = response.getOutputStream();
+        copyStream(inStream, writer, true); // Ignore errors, as browsers do weird things
+        writer.close();
+        return true;
+    }
 };
